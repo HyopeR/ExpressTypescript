6b690d7a035b5cfc11e6a97dedd51922012e3310
Tolgahan Çelik <tol_celik@hotmail.com>
2023-02-03 00:38:30 +0300

    Docker configuration added, typescript build issues fix.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:19-alpine AS builder
+RUN apk add --no-cache libc6-compat
+WORKDIR /usr/src/try-backend
+COPY . .
+RUN npm install
+RUN npm run build
+
+FROM node:19-alpine
+WORKDIR /usr/src/try-backend
+COPY --from=builder /usr/src/try-backend/package.json ./
+COPY --from=builder /usr/src/try-backend/dist ./dist
+RUN npm install -g patch-package
+RUN npm install --production
+ENV NODE_ENV production
+ENV PORT 3306
+EXPOSE 3306
+CMD ["npm", "start"]
---
+++ gulpfile.mjs
@@ -0,0 +1,30 @@
+import gulp from "gulp";
+import gulpTypescript from "gulp-typescript";
+import {deleteAsync} from "del";
+
+const tsProject = gulpTypescript.createProject("tsconfig.json");
+
+// Task which would transpile typescript to javascript
+gulp.task("typescript", function () {
+    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+});
+
+// Task which would delete the old dist directory if present
+gulp.task("clean", async function () {
+    return await deleteAsync("./dist");
+});
+
+// Task which would just create a copy of the current views directory in dist directory
+gulp.task("views", function () {
+    return gulp.src("./src/views/**/*.ejs").pipe(gulp.dest("./dist/views"));
+});
+
+// Task which would just create a copy of the current static assets directory in dist directory
+gulp.task("public", function () {
+    return gulp.src("./src/public/**/*").pipe(gulp.dest("./dist/public"));
+});
+
+// The default task which runs at start of the gulpfile.js
+gulp.task("default", gulp.series("clean", "typescript", "views", "public"), () => {
+    console.log("Done");
+});
